3e0814f786e41e31193da01c9bd63b2b59e9230f
Tobias Bühlmann <77664+tbuehlmann@users.noreply.github.com>
2026-05-21 09:52:24 +0200

    Update debian sid checksum

--- ext/or-tools/vendor.rb
+++ ext/or-tools/vendor.rb
@@ -47,7 +47,7 @@ else
     checksum = "c6c4341ff8f9aae3e77f161ca8ea3bb0d22f35ff696596fd11ec51c5da6bd4f7"
   elsif os == "debian" && os_info["VERSION_CODENAME"] == "forky"
     filename = "or-tools_amd64_debian-sid_cpp_v#{version}.tar.gz"
-    checksum = "3a248bef58439dbeee3e190f32384a63f090458ad3056c7b4d53bf5c2ba2df5e"
+    checksum = "66c31a4b38366df807e6572845d20ddc0ec82122487236590d1862a153bb0c4c"
   elsif os == "arch" && !arm
     filename = "or-tools_amd64_archlinux_cpp_v#{version}.tar.gz"
     checksum = "5505079f7b2a6d9379ba6ae446a3a639226d455ef1cfa32d2d23ffc4566e3a4b"
